a195a67296412359690b762e26e6d09d2dd3de9d
ParintornSompoo <s6301012610060@email.kmutnb.ac.th>
2021-08-27 14:23:08 +0700

    fixing unuse method and add mouse listener

--- Main.java
+++ Main.java
@@ -1,106 +1,72 @@
 package com.work;
 
-import javax.swing.*;
+import java.util.Arrays;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-
-public class Main {
-    static int countround = 0;
-    static JFrame frame = new JFrame();
-    static JPanel title_panel = new JPanel();
-    static JPanel button_panel = new JPanel();
-    static JLabel label = new JLabel();
-    static JButton[] buttons = new JButton[9];
-    static String player = "O";
-    static void guidisplay() {
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("OX Game");
-        frame.setSize(500,500);
-        frame.setLayout(new BorderLayout());
-        frame.setVisible(true);
-
-        label.setFont(new Font(Font.SERIF,Font.BOLD,30));
-        label.setHorizontalAlignment(JLabel.CENTER);
-        label.setText("Welcome to OX Game");
-
-        title_panel.setLayout(new BorderLayout());
-        title_panel.setBounds(0,0,500,10);
-        title_panel.add(label);
-        frame.add(title_panel,BorderLayout.NORTH);
-
-        button_panel.setLayout(new GridLayout(3,3));
-        button_panel.setBackground(new Color(150,150,150));
-
-        for(int i=0;i<9;i++) {
-            buttons[i] = new JButton();
-            button_panel.add(buttons[i]);
-            buttons[i].setText(" ");
-            buttons[i].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 120));
-            buttons[i].setFocusable(false);
-            buttons[i].addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    label.setText("This is X Turn");
-                    for (int x = 0; x < 9; x++) {
-                        if (e.getSource() == buttons[x]) {
-                            buttons[x].setText(player);
-                            buttons[x].setEnabled(false);
-                            if (player.equals("O")) {
-                                player = "X";
-                            } else {
-                                player = "O";
-                                label.setText("This is O Turn");
-                            }
-                            countround++;
-                            if (countround == 9)
-                                label.setText("DRAW");
-
-                        }
-                        checkwin();
-                    }
-                    if(checkwin()){
-                        for (int i = 0; i < 9; i++)
-                            buttons[i].setEnabled(false);
-                        if (player.equals("X"))
-                            label.setText("Player O WIN");
-                        if (player.equals("O"))
-                            label.setText("Player X WIN");
-                    }
-
-
-                }
-            });
+import java.awt.event.*;
+import javax.swing.*;
+public class Main extends JFrame implements MouseListener{
+    int size = 5;
+    int size_screen = 200*size;
+    int turnCount;
+    private final String[] player = new String[2];
+    private final String[][] board_array = new String[size][size];
+    JPanel screen = new JPanel();
+    public Main() {
+        this.turnCount = 0;
+        this.player[0] = "0";
+        this.player[1] = "X";
+        for (int i = 0;i < size;i++){
+            for (int j = 0;j < size;j++){
+                this.board_array[i][j] = " ";
+            }
         }
+        this.setTitle("OX Game");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
 
-        frame.add(button_panel);
+        screen.setPreferredSize(new Dimension(size_screen, size_screen));
+        screen.addMouseListener(this);
+        this.add(screen);
+        this.pack();
+        this.setVisible(true);
     }
-    static boolean checkwin(){
-        if (buttons[0].getText().equals(buttons[1].getText())&& buttons[1].getText().equals(buttons[2].getText()) && !buttons[0].getText().equals(" "))
-            return true;
-        else if (buttons[3].getText().equals(buttons[4].getText())&& buttons[4].getText().equals(buttons[5].getText()) && !buttons[3].getText().equals(" "))
-            return true;
-        else if (buttons[6].getText().equals(buttons[7].getText())&& buttons[7].getText().equals(buttons[8].getText()) && !buttons[6].getText().equals(" "))
-            return true;
-        else if (buttons[0].getText().equals(buttons[3].getText())&& buttons[3].getText().equals(buttons[6].getText()) && !buttons[0].getText().equals(" "))
-            return true;
-        else if (buttons[1].getText().equals(buttons[4].getText())&& buttons[4].getText().equals(buttons[7].getText()) && !buttons[1].getText().equals(" "))
-            return true;
-        else if (buttons[2].getText().equals(buttons[5].getText())&& buttons[5].getText().equals(buttons[8].getText()) && !buttons[2].getText().equals(" "))
-            return true;
-        else if (buttons[3].getText().equals(buttons[4].getText())&& buttons[4].getText().equals(buttons[5].getText()) && !buttons[3].getText().equals(" "))
-            return true;
-        else if (buttons[0].getText().equals(buttons[4].getText())&& buttons[4].getText().equals(buttons[8].getText()) && !buttons[0].getText().equals(" "))
-            return true;
-        else if (buttons[2].getText().equals(buttons[4].getText())&& buttons[4].getText().equals(buttons[6].getText()) && !buttons[2].getText().equals(" "))
-            return true;
-        else
-            return false;
+    public void add_position(int x,int y) {
+        if (this.board_array[y][x].equals(" ")) {
+            this.board_array[y][x] = this.player[0];
+            this.turnCount++;
+            this.change_player();
+        }
+    }
+    private void change_player() {
+        String swapplayer = this.player[0];
+        this.player[0] = this.player[1];
+        this.player[1] = swapplayer;
+    }
+    public boolean check_winner() {
+        return false;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
     public static void main(String[] args) {
-        guidisplay();
-        }
-
-
+        new Main();
+    }
 }
